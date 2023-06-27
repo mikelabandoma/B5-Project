fd98f5d0312eb27ef1f5afd1b0f53e9a2d0cdad4
Mikela Bandoma <mikelabandoma@gmail.com>
2023-06-27 14:15:36 -0700

    Initial Commit

---
+++ B5 Project/B5_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  B5_ProjectApp.swift
+//  B5 Project
+//
+//  Created by Mikela Bandoma on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct B5_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ B5 Project/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  B5 Project
+//
+//  Created by Mikela Bandoma on 6/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
